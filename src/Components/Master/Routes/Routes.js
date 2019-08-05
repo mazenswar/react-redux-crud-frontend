cdd88c8a35763b2f3cb0819d54843f9541a08bbc
Mazen Al Swar <43959520+mazenswar@users.noreply.github.com>
2019-08-05 15:34:16 -0400

    Update Routes.js

--- src/Components/Master/Routes/Routes.js
+++ src/Components/Master/Routes/Routes.js
@@ -6,11 +6,11 @@ import NotePages from "../../Notes";
 const Routes = () => {
   return (
     <Switch>
-      <Route exact path="/" component={NotePages.AllNotes} />
       <Route exact path="/notes" component={NotePages.AllNotes} />
       <Route exact path="/new-note" component={NotePages.NewNote} />
       <Route exact path="/notes/:id" component={NotePages.ShowNote} />
       <Route exact path="/notes/edit/:id" component={NotePages.EditNote} />
+      <Route component={NotePages.AllNotes} />
     </Switch>
   );
 };
